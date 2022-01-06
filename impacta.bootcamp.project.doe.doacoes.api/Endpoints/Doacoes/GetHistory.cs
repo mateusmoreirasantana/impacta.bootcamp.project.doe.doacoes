3c4de69836d91372f063aaaf6abd51bc78fb053f
mateus <m.mateus.moreira10@gmail.com>
2022-01-06 19:22:32 -0300

    add historico de doacoes

---
+++ impacta.bootcamp.project.doe.doacoes.api/Endpoints/Doacoes/GetHistory.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using impacta.bootcamp.project.doe.doacoes.api.Models.Doacoes;
+using impacta.bootcamp.project.doe.doacoes.infra.data.Data.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace impacta.bootcamp.project.doe.doacoes.api.Endpoints.Doacoes
+{
+    public class GetHistory : BaseEndPoint<List<HistoricoDoacoesResponse>>
+    {
+
+        private readonly SqlContext context;
+        public GetHistory(SqlContext sqlContext)
+        {
+            context = sqlContext;
+        }
+        [HttpGet("doacao/history")]
+        public async override Task<ActionResult<List<HistoricoDoacoesResponse>>> HandleAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+
+                var resp = await getAllResponse(((ClaimsIdentity)User.Identity).Claims.FirstOrDefault(cl => cl.Type == ClaimTypes.Email)?.Value);
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                string msg = $"Erro ao  campanha" + ex.ToString();
+
+                var resp = new HistoricoDoacoesResponse()
+                {
+                    status = "500",
+                    error = new Models.Errors.Error() { errorCode = "MODVAL01", errorMessage = msg }
+
+                };
+                return StatusCode(StatusCodes.Status500InternalServerError, resp);
+
+            }
+        }
+
+            private async Task<List<HistoricoDoacoesResponse>> getAllResponse(string user)
+        {
+            StringBuilder resp = new StringBuilder();
+            List<HistoricoDoacoesResponse> campanhaGetAllResponse = new List<HistoricoDoacoesResponse>();
+            using (SqlConnection conn = await context.GetConnection())
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("pr_historico_doacao_sel", conn))
+                {
+
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add("@username", System.Data.SqlDbType.VarChar);
+                    sqlCommand.Parameters["@username"].Value = user;
+
+                    
+                    if (sqlCommand.Connection.State.ToString() == "Closed")
+                    {
+                        conn.Open();
+                    }
+                    var sqlDataReader = sqlCommand.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    while (sqlDataReader.Read())
+                    {
+                        resp.Append(sqlDataReader.GetString(0));
+                    }
+                    Console.WriteLine(resp.ToString());
+
+
+                    campanhaGetAllResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<HistoricoDoacoesResponse>>(resp.ToString());
+                }
+
+
+                return campanhaGetAllResponse;
+            }
+        }
+
+    }
+}
---
+++ impacta.bootcamp.project.doe.doacoes.api/Models/Doacoes/HistoricoDoacoesResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace impacta.bootcamp.project.doe.doacoes.api.Models.Doacoes
+{
+    public class HistoricoDoacoesResponse:BaseResponse
+    {
+        public bool exibirValorDoado { get; set; }
+        public double valorDoado { get; set; }
+        public bool exibirNomeDoador { get; set; }
+        public string nomeDoador { get; set; }
+        public DateTime dataDoacao { get; set; }
+        public Campanhas campanhas { get; set; }
+    }
+
+    public class SolicitacaoVoluntario
+    {
+        public bool? status { get; set; }
+        public string descricaoVaga { get; set; }
+    }
+
+    public class Campanhas
+    {
+        public string img_background { get; set; }
+        public string img_background_card { get; set; }
+        public int id_campanha { get; set; }
+        public string nome_campanha { get; set; }
+        public string desc_campanha { get; set; }
+        public double valorTotal { get; set; }
+        public string tipoCampanha { get; set; }
+        public object dataLimite { get; set; }
+        public bool status { get; set; }
+        public double valorAtual { get; set; }
+        public object organizacao { get; set; }
+        public SolicitacaoVoluntario solicitacaoVoluntario { get; set; }
+        public string tipo_doacao { get; set; }
+        public string unidadeMedida { get; set; }
+    }
+
+
+}
